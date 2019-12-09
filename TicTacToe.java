b8936e06a54f4ebf551e14c8b18da97327716c9c
IvanSkotar <44509499+IvanSkotar@users.noreply.github.com>
2019-12-09 14:30:32 -0800

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class TicTacToe{
    static int row = 0;
    static int col = 0;
+	
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int index = 1;
@@ -12,6 +13,7 @@ public class TicTacToe{
                       { ' ', ' ', ' '}
                     };
     printGrid(grid);
+	  
     while (!isWinner(grid, player)){
       if(index % 2 == 0){
         player = 'O';
@@ -24,18 +26,19 @@ public class TicTacToe{
         playTurn(input, player);
       }
       setGridValue(grid, player, row, col);
-			printGrid(grid);
-			if (isWinner(grid, player)){
-			  System.out.printf("Player " + player + " wins!!!");
-			  System.exit(0);
-			}
-			if (isDraw(grid)){
-			  System.out.printf("It is a draw!!!");
-			  System.exit(0);
-			}
-			index++;
+	printGrid(grid);
+	if (isWinner(grid, player)){
+	  System.out.printf("Player " + player + " wins!!!");
+	  System.exit(0);
+	}
+	if (isDraw(grid)){
+	  System.out.printf("It is a draw!!!");
+	System.exit(0);
+	}
+	  index++;
   	} 
   }
+	
   // print a tic tac toe grid  
   public static void printGrid(char[][] grid) {
     System.out.println("+---+---+---+");
@@ -48,6 +51,7 @@ public class TicTacToe{
     System.out.println("+---+---+---+");
     }
   }
+	
   // set a grid value, row and column are 0, 1, or 2
   public static void setGridValue(char[][] grid, char value, int row, int col) {
     if(value == 'X' || value == 'O'){
@@ -57,8 +61,9 @@ public class TicTacToe{
       System.out.println("Wrong value");
     }
   }
+	
+  // Check if someone won	
   public static boolean isWinner(char[][] grid, char value) {
-  // Check if someone won
     if(grid[0][0] == value && grid[0][1] == value && grid[0][2] == value)
       return true;
     if(grid[1][0] == value && grid[1][1] == value && grid[1][2] == value)
@@ -77,14 +82,16 @@ public class TicTacToe{
       return true;
     return false;
   }
-  public static boolean isPositionTaken(char[][] grid, int row, int col) {
+
   // Check if pisition taken
+  public static boolean isPositionTaken(char[][] grid, int row, int col) {
     if(grid[row - 1][col - 1] == 'X' || grid[row - 1][col - 1] == 'O'){
       System.out.println("Position taken. Try again.");
       return true;
     }
     return false;
   }
+	
   // Check if all nine spots are taken
   public static boolean isDraw(char[][] grid) {
     int emptyCels = 0;
@@ -100,19 +107,21 @@ public class TicTacToe{
     }
     return false;
   }
+	
+  // player turn	
   public static void playTurn(Scanner input, char player){
     System.out.print("Player " + player + ", enter row and column: ");
     if (input.hasNextInt()){
       row = input.nextInt();
-			col = input.nextInt();
-		}
-		else {
-		  System.out.print("Not a number. Exiting.");
-			System.exit(0);
-		}
-		if ((row < 1 || row > 3) || (col < 1 || col > 3)){
-		  System.out.println("Value not 1,2 or 3. Exiting.");
+      col = input.nextInt();
+    }
+    else {
+      System.out.print("Not a number. Exiting.");
       System.exit(0);
-		}
+    }
+    if ((row < 1 || row > 3) || (col < 1 || col > 3)){
+      System.out.println("Value not 1,2 or 3. Exiting.");
+      System.exit(0);
+    }
   }
 }
